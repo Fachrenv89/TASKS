412a569e5b340e738c9daf6caa9a9adc0457027c
SemmiDev <sammidev4@gmail.com>
2021-04-05 20:18:33 +0700

    ONE PYTHON

---
+++ PYTHON/one.py
@@ -0,0 +1,16 @@
+a = 1
+b = -5
+c = 6
+
+D = b * b - 4 * a * c
+
+if D < 0:
+	print("AKAR IMAJINER")
+elif D == 0:
+	print("1 AKAR REAL")
+else:
+	print("2 AKAR REAL")
+
+x1 = 2
+x2 = 3
+print(f"hasil akar (x^2 - 5x + 6) => x1 = {x1}, x2 = {x2}")
